0c7078d925d13dfaf7a62141ee236ead4e5fdb43
Melisa <melisabe@ucm.es>
2023-02-03 12:25:03 +0100

    Reloj prueba2

---
+++ prueba2.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter.ttk import * 
+from time import strftime
+
+def hora():
+	string = strftime('%H:%M:%S %p')
+	etiqueta.config(text=string)
+	etiqueta.after(1000, hora)
+ 
+ventana = Tk()
+ventana.title('Hora') 
+etiqueta = Label(ventana, font=('arial', 20, 'bold'), background='black', foreground='white') 
+etiqueta.pack(anchor='center')
+hora() 
+mainloop()
